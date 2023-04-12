907def45f1584810c26aa46ee201d0e3872f008a
MuhSaktiR <seken876@gmail.com>
2023-04-12 11:32:16 +0700

    membuat public member untuk void input method class Mahasiswa

--- Latihan_OOP/Latihan_OOP.cpp
+++ Latihan_OOP/Latihan_OOP.cpp
@@ -6,4 +6,14 @@ private:
 	int nim;
 	string nama;
 	float nilai;
+
+public:
+	void input() {
+		cout << "Masukkan NIM \t: ";
+		cin >> nim;
+		cout << "Masukkan Nama \t: ";
+		cin >> nama;
+		cout << "Masukkan nilai \t: ";
+		cin >> nilai;
+	}
 };
